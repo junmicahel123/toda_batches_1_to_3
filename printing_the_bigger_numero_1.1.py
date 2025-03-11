fd8b1f53b66eec096899f73bfff9c7878c275343
Owen <juanmig134187@gmail.com>
2025-03-11 22:16:49 +0800

    asking_the_user_for_numero_1.1

---
+++ printing_the_bigger_numero_1.1.py
@@ -0,0 +1,10 @@
+#ask the user to input two numbers (float)
+
+numero_uno = float(input("What's the first number?:  "))
+numero_dos = float(input("What's the second number?:  "))
+
+
+# evaluate the numbers
+    #use If statements
+    # evaluate using < > function
+# print results

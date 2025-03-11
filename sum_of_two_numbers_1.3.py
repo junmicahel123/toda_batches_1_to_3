4fd2c07d6fd3f58ef1feb59f5f516c6ce3e2c40d
Owen <juanmig134187@gmail.com>
2025-03-11 22:46:28 +0800

    adding_the_two_input_numbers_1.3

--- sum_of_two_numbers_1.3.py
+++ sum_of_two_numbers_1.3.py
@@ -1,12 +1,13 @@
 #Pseudocode
 # ask user to input two numbers
-
+# convert to float
 
 numero_one = float(input("What's the first number?:  "))
 numero_two = float(input("What's the second number?:  "))
 
-
-
-# convert to float
 # add two numbers
+
+resulta = numero_one + numero_two
 # print results
+
+

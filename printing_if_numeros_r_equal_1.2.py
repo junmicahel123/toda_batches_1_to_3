a68a17533433138131f5955b3a2afa09355bd94e
Owen <juanmig134187@gmail.com>
2025-03-11 22:37:43 +0800

    evaluation_using_if_statements_with_results_1.2

--- printing_if_numeros_r_equal_1.2.py
+++ printing_if_numeros_r_equal_1.2.py
@@ -4,8 +4,16 @@
 numero_1 = float(input("What's the first number?:  "))
 numero_2 = float(input("What's the second number?:  "))
 
+
 # use if statements
     #use "==" operations
 # print results
+
+if numero_1 == numero_2:
+    print("Equal")
+
+else:
+    print("Not Equal")
+
 
 

e460ef5b54f68125b4021907ef7bd78593251283
Owen <juanmig134187@gmail.com>
2025-03-11 22:43:30 +0800

    user_input_two_numbers_1.3

---
+++ sum_of_two_numbers_1.3.py
@@ -0,0 +1,12 @@
+#Pseudocode
+# ask user to input two numbers
+
+
+numero_one = float(input("What's the first number?:  "))
+numero_two = float(input("What's the second number?:  "))
+
+
+
+# convert to float
+# add two numbers
+# print results

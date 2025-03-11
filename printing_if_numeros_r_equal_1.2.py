85ee79a188c79f139dcf4600e1ce8d00a456350c
Owen <juanmig134187@gmail.com>
2025-03-11 22:32:44 +0800

    ask_for_number_inputs_1.2

--- printing_if_numeros_r_equal_1.2.py
+++ printing_if_numeros_r_equal_1.2.py
@@ -1,5 +1,9 @@
 # ask to input two numbers 
 # convert to float
+
+numero_1 = float(input("What's the first number?:  "))
+numero_2 = float(input("What's the second number?:  "))
+
 # use if statements
     #use "==" operations
 # print results

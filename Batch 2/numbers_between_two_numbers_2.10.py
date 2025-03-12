2f605d2255c14c9965dc09343346f3176f6558f3
Owen <juanmig134187@gmail.com>
2025-03-13 01:02:57 +0800

    print numbers in between with correction if num1>num2_2.10

--- Batch 2/numbers_between_two_numbers_2.10.py
+++ Batch 2/numbers_between_two_numbers_2.10.py
@@ -4,3 +4,18 @@
 # use if to make sure
     # if first number is bigger then second
 # print results
+
+first_number = 0
+second_number = 0
+
+first_number = int(input("Input first number:: "))
+second_number = int(input("Input second number:: "))
+
+if first_number > second_number:
+    first_number, second_number = second_number, first_number
+
+
+print(f"Numbers between {first_number} and {second_number}:")
+for numbers in range(first_number + 1, second_number):
+    print(numbers)
+    

3aac5d5757c2f86c1bf0bfbe5020e989171f0587
Owen <juanmig134187@gmail.com>
2025-03-12 22:18:59 +0800

    while_loop_for_input_and_checking_3.4

--- Batch 3/display_the_lowest_number_3.4.py
+++ Batch 3/display_the_lowest_number_3.4.py
@@ -1,10 +1,19 @@
 # pseudocode
 # initialize  number set []
+
+number_set = []
+
 # use while loop for 10 inputs
 # use try and except for invalid input checking
 
 # ask user for input numbers
 # add number value to set
-
+while len(number_set) < 10:
+    try: 
+        number = int(input("Enter a number:: "))
+        number_set.append(number)
+    except ValueError:
+        print("Invalid input. restart")
+        break
 # use min function
 

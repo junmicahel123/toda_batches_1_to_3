da1b8ad590cd6886b6be171d56cc2ad1fdebe7a4
Owen <juanmig134187@gmail.com>
2025-03-12 13:06:54 +0800

    input_two_numbers_2.5

--- remainder_of_the_first_number_2.5.py
+++ remainder_of_the_first_number_2.5.py
@@ -6,5 +6,10 @@ seconD_number = 0
 
 
 # ask for two user inputs
+
+firsT_number = float(input("Please input first number ::"))
+seconD_number = float(input("Please input second number::"))
+
+
 # use modulus operation "%" for the remainder
 # print the remainder

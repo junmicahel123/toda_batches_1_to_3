bbccbfc67e3859ec4b0ef046e6fad4ff140b2910
Owen <juanmig134187@gmail.com>
2025-03-13 00:49:30 +0800

    pseudocode na maangas_2.6

---
+++ Batch 2/first_num_minus_the_rest_of_10_number_2.6.py
@@ -0,0 +1,12 @@
+# pseudocode
+# initialize constants 
+    # first_number
+    # the_rest_of_the_numbers
+
+# ask user for inputs of the 10 numbers
+    # user for loop for the 9 other numbers
+    # add the numbers to the initialized constant
+    # separated from the first number
+
+# minus the first number to the iniatilized constant
+# print the results

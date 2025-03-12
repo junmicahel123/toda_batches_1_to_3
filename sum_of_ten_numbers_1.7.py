5b5c2c4e0290fd91e1479cab01155b0274bd1738
Owen <juanmig134187@gmail.com>
2025-03-12 08:45:11 +0800

    printing_total_sum_plus_minor_changes_1.7

--- sum_of_ten_numbers_1.7.py
+++ sum_of_ten_numbers_1.7.py
@@ -6,9 +6,8 @@ total_sum = 0
     # the input numbers will be added to the "total_sum"
 for number_input in range(10):
     numbers = float(input(f" Enter the number {number_input + 1}:  "))
-    total_sum += number_input
+    total_sum += numbers
 
 # print the total sum of ten numbers
 
-
-
+print(f" The total sum of all ten numbers is -->>{total_sum}")

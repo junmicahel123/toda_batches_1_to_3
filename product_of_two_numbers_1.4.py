3ed5e83261d2128f3d0c7856ab368453957aa3b0
Owen <juanmig134187@gmail.com>
2025-03-12 08:10:39 +0800

    forgot_to_put_float_line_6_1.4

--- product_of_two_numbers_1.4.py
+++ product_of_two_numbers_1.4.py
@@ -2,15 +2,11 @@
 # ask user to input two numbers
 # Convert to float
 
-
 number_1 = float(input("What's the first Number?: "))
-number_2 = input(("What's the second Number?: "))
+number_2 = float(input("What's the second Number?: "))
 
- 
 # Multiply the input numbers
-
 produkto = number_1 * number_2
-
 
 # print results
 

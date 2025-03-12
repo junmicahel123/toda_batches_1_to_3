4e487954eeaca42ac96d436949bd63fe12728c53
Owen <juanmig134187@gmail.com>
2025-03-12 21:46:03 +0800

    pseudocode

---
+++ Batch 3/display_all_numbers_and_only_one_duplicate_3.2.py
@@ -0,0 +1,12 @@
+# pseudocode
+# initialize 
+    # numbers []
+    # set ()
+    # filtered [] (only one duplicates)
+
+# use for loop for the inpts
+#  add input to number [] list
+# number not in set ()
+    # add to filtered set
+
+# print result

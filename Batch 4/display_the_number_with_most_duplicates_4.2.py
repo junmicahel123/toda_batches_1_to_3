98e1ed7eb1fbf4bb06b3baad03c7658d95f2a61c
Owen <juanmig134187@gmail.com>
2025-03-12 22:46:19 +0800

    initialization and while looping for checking and input numbers_4.2

--- Batch 4/display_the_number_with_most_duplicates_4.2.py
+++ Batch 4/display_the_number_with_most_duplicates_4.2.py
@@ -1,5 +1,16 @@
 # pseudocode
 # initialize set []
+numbers = []
+
+while len(numbers) < 10:
+    try:
+        number_input = int(input("Enter number ::"))
+        numbers.append(number_input)
+    except ValueError:
+        print("Invalid input. Restart the program to try again")
+        break
+    
+
 # use while loop for number inputs 10
 # try for checking input
 # if numbers 

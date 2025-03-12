f3f917dbd8e84051499824d016aae8cac8c9630f
Owen <juanmig134187@gmail.com>
2025-03-12 13:28:50 +0800

    initialize_constant_2.6

--- first_number_minus_the_other_nine_numbers_2.6.py
+++ first_number_minus_the_other_nine_numbers_2.6.py
@@ -2,6 +2,10 @@
 # initialize constants 
     # first_number
     # the_rest_of_the_numbers
+
+first_number = 0
+the_rest_of_the_numbers = 0
+
 # ask user for inputs of the 10 numbers
     # user for loop for the 9 other numbers
     # add the numbers to the initialized constant

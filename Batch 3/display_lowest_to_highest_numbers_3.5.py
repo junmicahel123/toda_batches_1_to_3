933a24381cb280ff335bfe747a425e62b167e1d8
Owen <juanmig134187@gmail.com>
2025-03-12 22:23:13 +0800

    pseudocode_3.5

---
+++ Batch 3/display_lowest_to_highest_numbers_3.5.py
@@ -0,0 +1,8 @@
+# pseudocode 
+# initialize number list constant []
+# use while loop for number input 10
+# use try and except for value checking
+# ask for input values
+# add to number list
+# sort numbers
+# print the sorted numbers

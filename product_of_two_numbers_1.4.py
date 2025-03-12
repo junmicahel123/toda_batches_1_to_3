41d4251102628561567c61a3373150c10628c0dd
Owen <juanmig134187@gmail.com>
2025-03-12 08:04:46 +0800

    string_to_float_conversion_1.4

--- product_of_two_numbers_1.4.py
+++ product_of_two_numbers_1.4.py
@@ -1,10 +1,12 @@
 # Pseudocode
 # ask user to input two numbers
+# Convert to float
 
-number_1 = input("What's the first Number?: ")
-number_2 = input("What's the second Number?: ")
 
-# Convert to float 
+number_1 = float(input("What's the first Number?: "))
+number_2 = input(("What's the second Number?: "))
+
+ 
 # Multiply the input numbers
 # print results
 

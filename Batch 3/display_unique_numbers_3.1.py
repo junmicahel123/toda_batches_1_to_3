f48a24ad2496fdf294608d37344b3def675e157d
Owen <juanmig134187@gmail.com>
2025-03-12 21:41:21 +0800

    results_printing_3.1

--- Batch 3/display_unique_numbers_3.1.py
+++ Batch 3/display_unique_numbers_3.1.py
@@ -2,7 +2,7 @@
 # initialize numbers = []
 # initialize unique numbers = []
 number_list = []
-unique_numbers_list = []
+
 
 
 
@@ -12,9 +12,12 @@ unique_numbers_list = []
 for numb in range(10):
     numbers = int(input(f"Enter number {numb + 1} ::  "))
     number_list.append(numbers)
+
+unique_numbers_list = []    
 # if number.count == 1 
     # add to unique number list = []
-for num in number_list:
+for numbers in number_list:
     if number_list.count(numbers) == 1:
         unique_numbers_list.append(numbers)
 # print(unque numbers)
+print("Unique numbers are ::", unique_numbers_list)

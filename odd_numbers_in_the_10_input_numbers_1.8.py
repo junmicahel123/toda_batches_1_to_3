b7238f4f38238abe7da038998bfdf26e2ad18143
Owen <juanmig134187@gmail.com>
2025-03-12 08:51:11 +0800

    initialization_of_total_odd_numbers_1.8

--- odd_numbers_in_the_10_input_numbers_1.8.py
+++ odd_numbers_in_the_10_input_numbers_1.8.py
@@ -1,5 +1,8 @@
 # Pseudocode
 # initialization of "total_odd_numbers"
+
+total_odd_numbers = 0
+
 #use for loop to prompt 10 input numbers
 # if the number input is odd 
     #add 1 to the total_odd_numbers

a4fc5e2bc4446f1b2d6c0f623650e8a980cadd6e
Owen <juanmig134187@gmail.com>
2025-03-12 08:03:20 +0800

    user_input_two_numbers_1.4

--- product_of_two_numbers_1.4.py
+++ product_of_two_numbers_1.4.py
@@ -1,5 +1,9 @@
 # Pseudocode
 # ask user to input two numbers
+
+number_1 = input("What's the first Number?: ")
+number_2 = input("What's the second Number?: ")
+
 # Convert to float 
 # Multiply the input numbers
 # print results

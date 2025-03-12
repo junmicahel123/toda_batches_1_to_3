5194b7feeae9bca4f565170fb0258527eded4b76
Owen <juanmig134187@gmail.com>
2025-03-13 00:45:13 +0800

    division to find the quotient_2.4

--- Batch 2/quotient_of_2_numbers_no_decimals_2.4.py
+++ Batch 2/quotient_of_2_numbers_no_decimals_2.4.py
@@ -4,3 +4,15 @@
 # find quotient
     # convert to INTEGER to remove decimal
 # print quotient
+
+numero_uno = 0
+numero_dos = 0
+
+numero_uno = int(input("Input number 1 ::"))
+numero_dos = int(input("Input number 2 ::"))
+
+quotient = int(numero_uno / numero_dos)
+
+print(f"The quotient of {numero_uno} and {numero_dos} is -->> {quotient}")
+
+

06a48fd68f1d87d954d3a86f88acd2d0f580de37
Owen <juanmig134187@gmail.com>
2025-03-12 08:28:25 +0800

    initialization_of_values_1.6

---
+++ raising_the_first_number_using_second_number_1.6.py
@@ -0,0 +1,10 @@
+# pseudocode 
+# initialization
+
+base_number = 0
+exponent_number = 0
+
+# ask user to input two numbers
+# use the second number as exponent and the first as the base
+# base ** exponent
+# print result

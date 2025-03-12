3d5fa47cfe44a79d5c05bd038adb951ec3d18be4
Owen <juanmig134187@gmail.com>
2025-03-12 23:49:23 +0800

    pseudocode with initializations_1.6

---
+++ Batch 1/raising_the_first_num_with_2nd_numb_1.6.py
@@ -0,0 +1,10 @@
+# pseudocode 
+# initialization
+# ask user to input two numbers (float)
+# use the second number as exponent and the first as the base
+# base ** exponent
+
+# print result
+base_number = 0
+exponent_number = 0
+

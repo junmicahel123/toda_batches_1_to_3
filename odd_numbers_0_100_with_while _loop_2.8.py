28339c68dc51905631dfd6ef318a04f6c3b0e847
Owen <juanmig134187@gmail.com>
2025-03-12 14:28:21 +0800

    add_2_to_the constant_2.8

--- odd_numbers_0_100_with_while _loop_2.8.py
+++ odd_numbers_0_100_with_while _loop_2.8.py
@@ -6,8 +6,9 @@ numbers = 1
 
 # use while loop
     # while numbers <= 100
+# print the number 
 
 while numbers <= 100:
     print(f"Odd numbers {numbers}")
     # num += 2
-# print the number 
+    numbers += 2

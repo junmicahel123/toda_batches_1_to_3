a82db098b574a6ca1e1facd18813ffa2b417f413
Owen <juanmig134187@gmail.com>
2025-03-12 23:51:52 +0800

    exponent into base printing_1.6

--- Batch 1/raising_the_first_num_with_2nd_numb_1.6.py
+++ Batch 1/raising_the_first_num_with_2nd_numb_1.6.py
@@ -8,3 +8,10 @@
 base_number = 0
 exponent_number = 0
 
+base_number = float(input("What's the first number?: "))
+exponent_number = float(input("What's the second number?: "))
+
+resulting_value = base_number ** exponent_number
+
+print(f"The result of raising {base_number} to {exponent_number} is -->> {resulting_value}")
+

8937ebd4776919f23cf43ec6ad273ce7e7aff603
Owen <juanmig134187@gmail.com>
2025-03-13 00:00:04 +0800

    total number of odd numbers printing_1.8

--- Batch 1/odd_numbers_from_10_inputs_1.8.py
+++ Batch 1/odd_numbers_from_10_inputs_1.8.py
@@ -6,3 +6,12 @@
 
 #print total_odd_numbers
 
+total_odd_numbers = 0
+
+for numbers in range(10):
+    number_input = float(input(f"Input number {numbers + 1}: "))
+
+    if number_input % 2 != 0:
+        total_odd_numbers += 1
+
+print(f" The total number of odd in the 10 numbers is -->>  {total_odd_numbers}")

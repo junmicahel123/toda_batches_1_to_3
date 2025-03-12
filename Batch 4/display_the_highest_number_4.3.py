48adfa81214b75c2db92f0a6d48662ae559448cb
Owen <juanmig134187@gmail.com>
2025-03-12 22:51:36 +0800

    initialize list and while loop for inputs _4.3

--- Batch 4/display_the_highest_number_4.3.py
+++ Batch 4/display_the_highest_number_4.3.py
@@ -1,7 +1,14 @@
 # pseudocode 
 # initialize the set number []
-
+number_list = []
 # use while loop for input number
+while len(number_list) < 10: 
+    try:
+        number = int(input("Input a number::"))
+        number_list.append(number)
+    except ValueError:
+        print("Invalid input. Restart to try again")
+        
 # use try loop to check valid inputs
 # use max function for number set
 

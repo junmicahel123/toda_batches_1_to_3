dd3da6c508323ffcb3a234c7e64edf98c066b813
Owen <juanmig134187@gmail.com>
2025-03-12 22:25:26 +0800

    while_loop_input_chekcin_input_3.5

--- Batch 3/display_lowest_to_highest_numbers_3.5.py
+++ Batch 3/display_lowest_to_highest_numbers_3.5.py
@@ -1,8 +1,17 @@
 # pseudocode 
 # initialize number list constant []
+number_list = []
 # use while loop for number input 10
 # use try and except for value checking
 # ask for input values
 # add to number list
+
+while len(number_list) < 10:
+    try:
+        number_input = int(input("Enter a number ::"))
+        number_list.append(number_input)
+    except ValueError:
+        print("Invalid input .Restart the program")
+        break
 # sort numbers
 # print the sorted numbers

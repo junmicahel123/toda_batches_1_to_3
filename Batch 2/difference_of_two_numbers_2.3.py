e64d010372e384febe167906ff777b7f54e2174e
Owen <juanmig134187@gmail.com>
2025-03-12 14:03:42 +0800

    Nag move po ulit

---
+++ Batch 2/difference_of_two_numbers_2.3.py
@@ -0,0 +1,20 @@
+# pseudocode
+# initialization of constants
+
+first_number = 0
+second_number = 0
+
+
+# ask for two input numbers
+
+first_number = float(input("Input first number ::"))
+second_number = float(input("Input second number::"))
+
+# use - function to find the difference 
+
+difference = first_number - second_number
+
+# print the difference
+
+print(f"The difference of {first_number} and {second_number} is -->> {difference}")
+

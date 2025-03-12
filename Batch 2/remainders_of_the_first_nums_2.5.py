86cdd9b0adb3f888270c4e8358e239a6af796d29
Owen <juanmig134187@gmail.com>
2025-03-13 00:47:34 +0800

    printing the remainder_2.5

--- Batch 2/remainders_of_the_first_nums_2.5.py
+++ Batch 2/remainders_of_the_first_nums_2.5.py
@@ -3,3 +3,14 @@
 # ask for two user inputs
 # use modulus operation "%" for the remainder
 # print the remainder
+firsT_number = 0
+seconD_number = 0
+
+firsT_number = float(input("Please input first number ::"))
+seconD_number = float(input("Please input second number::"))
+
+remainder = firsT_number % seconD_number
+
+print(f"The remainder of {firsT_number} and {seconD_number} is -->> {remainder}")
+
+

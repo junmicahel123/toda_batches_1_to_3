533a6b2e0bd104a1f7fe9923a3b2c97e95975283
Owen <juanmig134187@gmail.com>
2025-03-13 00:38:00 +0800

    finding the smaller number_2.1

--- Batch 2/identify_the_smaller_num_2.1.py
+++ Batch 2/identify_the_smaller_num_2.1.py
@@ -6,3 +6,12 @@
 
 # use "min" funnctio for (firs_number , second_number)
 # print the results
+first_number = 0
+second_number = 0
+
+first_number = float(input("What's the first number?:"))
+second_number= float(input("What's the second number?:"))
+
+smaller_number = min(first_number, second_number)
+
+print(f"The smaller number is {smaller_number}")

08aff2d69dfd8f0a3289d729ef74f6afec008cc6
Owen <juanmig134187@gmail.com>
2025-03-12 23:23:58 +0800

    implementation using if statements and > func_1.1

--- Batch 1/printing_the_bigger_numero_1.1.py
+++ Batch 1/printing_the_bigger_numero_1.1.py
@@ -3,4 +3,11 @@
     #use If statements
     # evaluate using < > function
 # print results
+numero_uno = float(input("What's the first number?:  "))
+numero_dos = float(input("What's the second number?:  "))
+
+if numero_uno > numero_dos:
+    print(f"{numero_uno} is the bigger number :) ")
+else:
+    print(f"{numero_dos} is the bigger number :) ")
 

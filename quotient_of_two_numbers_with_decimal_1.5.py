476d6ac63bb17675bd10b70f06905caecac2223e
Owen <juanmig134187@gmail.com>
2025-03-12 08:17:50 +0800

    user_input_dalawang_numbers_1.5

--- quotient_of_two_numbers_with_decimal_1.5.py
+++ quotient_of_two_numbers_with_decimal_1.5.py
@@ -1,5 +1,9 @@
 # pseudocode
 # ask for number input
+
+first_number = input("What's the first number?: ")
+second_number = input("What's the second number?: ")
+
 # convert to float
 # use / function to find quotient
 # print quotient

22ec40efa767b033a51f2417d8fd12763bc56f8e
Owen <juanmig134187@gmail.com>
2025-03-12 21:09:51 +0800

    pseudocode_3.1

---
+++ Batch 3/display_unique_numbers_3.1.py
@@ -0,0 +1,12 @@
+# pseudocode
+
+# use append function
+# initialize set
+    # numbers_set = []
+# use for loop for 10 input numbers
+# append number input
+#  initialize set for unique numbers
+#  use for loop 
+    # if numbers.count(num) == 1
+    # add to unique set
+# print unique set

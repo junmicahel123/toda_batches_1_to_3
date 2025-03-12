266eb63cadc92c483d27c9d6df91683218ded9ec
Owen <juanmig134187@gmail.com>
2025-03-12 14:22:08 +0800

    while_loop_for_odd_number_2.8

--- odd_numbers_0_100_with_while _loop_2.8.py
+++ odd_numbers_0_100_with_while _loop_2.8.py
@@ -1,7 +1,14 @@
 # pseudocode
 # initialize constant 
     # number = 1
+
+numbers = 1
+
 # use while loop
     # while numbers <= 100
+
+while numbers <= 100:
+    print(f"Odd numbers are {numbers}")
+    
     # num += 2
 # print the number 

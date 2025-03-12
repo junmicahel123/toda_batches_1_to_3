16f826b227e8766fb3ccc7480d1ad54ab4660b49
Owen <juanmig134187@gmail.com>
2025-03-12 21:50:17 +0800

    for loops and if statements to add to the sets_3.2

--- Batch 3/display_all_numbers_and_only_one_duplicate_3.2.py
+++ Batch 3/display_all_numbers_and_only_one_duplicate_3.2.py
@@ -4,9 +4,20 @@
     # set ()
     # filtered [] (only one duplicates)
 
+number_list = []
+seen = set()
+filtered_numbers = []
+
+
 # use for loop for the inpts
-#  add input to number [] list
-# number not in set ()
-    # add to filtered set
+    #  add input to number [] list
+    # number not in set ()
+         # add to filtered set
+for num in range(10):
+    numbers = int(input(f"Enter Number {num + 1 }: "))
+    number_list.append(numbers)
+    if numbers not in seen:
+        filtered_numbers.append(numbers)
+     
 
 # print result

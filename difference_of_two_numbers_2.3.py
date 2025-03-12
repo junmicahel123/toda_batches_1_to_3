61b0f2cd3e0c87a87046a5654b63b363483e4150
Owen <juanmig134187@gmail.com>
2025-03-12 12:36:48 +0800

    intialization_of_constants_2.3

--- difference_of_two_numbers_2.3.py
+++ difference_of_two_numbers_2.3.py
@@ -1,5 +1,10 @@
 # pseudocode
 # initialization of constants
+
+first_number = 0
+second_number = 0
+
+
 # ask for two input numbers
 # use - function to find the difference 
 # print the difference

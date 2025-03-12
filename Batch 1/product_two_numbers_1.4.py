10943d2fb672ff53be07e4fb691c32e7830bbe6a
Owen <juanmig134187@gmail.com>
2025-03-12 23:44:43 +0800

    printing the sum _1.4

--- Batch 1/product_two_numbers_1.4.py
+++ Batch 1/product_two_numbers_1.4.py
@@ -3,3 +3,12 @@
 # Convert to float
 # Multiply the input numbers
 # print results
+
+number_1 = float(input("What's the first Number?: "))
+number_2 = float(input("What's the second Number?: "))
+
+produkto = number_1 * number_2
+
+
+
+print(f"The product of {number_1} and {number_2} is {produkto} :)")

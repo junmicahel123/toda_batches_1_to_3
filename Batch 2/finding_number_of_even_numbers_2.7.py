398873bc3ed67bac184b77f86d60a7dd24f8a8a4
Owen <juanmig134187@gmail.com>
2025-03-13 00:55:26 +0800

    print even count by using sets to list the count_2.7

--- Batch 2/finding_number_of_even_numbers_2.7.py
+++ Batch 2/finding_number_of_even_numbers_2.7.py
@@ -9,3 +9,14 @@
 
 # print even_numbers
 
+count_even_numbers = 0
+
+
+for number_input in range(10):
+    numbers = int(input(f"Input Number {number_input + 1}::  "))
+
+    if numbers % 2 != 1:
+        count_even_numbers += 1
+
+print(f"The even number count is {count_even_numbers}")
+

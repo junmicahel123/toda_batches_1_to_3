ff0d71bcb971299b1228ecdb2d76963da6cdc972
Owen <juanmig134187@gmail.com>
2025-03-13 00:42:40 +0800

    difference finding _2.3

--- Batch 2/difference_of_2_.number_2.3.py
+++ Batch 2/difference_of_2_.number_2.3.py
@@ -5,3 +5,14 @@
 # use - function to find the difference 
 
 # print the difference
+
+first_number = 0
+second_number = 0
+
+first_number = float(input("Input first number ::"))
+second_number = float(input("Input second number::"))
+
+difference = first_number - second_number
+
+print(f"The difference of {first_number} and {second_number} is -->> {difference}")
+

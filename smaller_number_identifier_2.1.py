88bfa333525d38bd1f1eda0660f09fc54c0dd36c
Owen <juanmig134187@gmail.com>
2025-03-12 12:19:05 +0800

    intializing_constants_1_2_2.1

--- smaller_number_identifier_2.1.py
+++ smaller_number_identifier_2.1.py
@@ -2,5 +2,9 @@
 # initializing constants
     # first_number = 0
     # second_number = 0
+
+first_number = 0
+second_number = 0
 # use "min" funnctio for (firs_number , second_number)
+
 # print the results

3bc2fd7407623a8d9e94bb0e3ed6f8ac70207c66
Owen <juanmig134187@gmail.com>
2025-03-12 23:56:05 +0800

    pseudocode fast_1.7

---
+++ Batch 1/sum_of_the_ten_number_1.7.py
@@ -0,0 +1,5 @@
+# pseudocode
+# initialize the "total_sum" as 0
+# use "for" loop to ask user to input ten numbers
+    # the input numbers will be added to the "total_sum"
+# print the total sum of ten numbers

56b70e365a63f71ac666975af1b0039e30cdde9d
Owen <juanmig134187@gmail.com>
2025-03-12 23:41:07 +0800

    printing the sum_1.3

--- Batch 1/sum_two_numbers_1.3.py
+++ Batch 1/sum_two_numbers_1.3.py
@@ -4,3 +4,13 @@
 # add two numbers
 
 # print results
+
+numero_one = float(input("What's the first number?:  "))
+
+numero_two = float(input("What's the second number?:  "))
+
+
+resulta = numero_one + numero_two
+
+
+print(f"The sum of two numbers is -->> {resulta}")

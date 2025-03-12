e50333717b3267203f699ba858063a88eedb88a1
Owen <juanmig134187@gmail.com>
2025-03-12 23:06:19 +0800

    inputing values using while loop_4.5

--- Batch 4/display_average_of_all_number_4.5.py
+++ Batch 4/display_average_of_all_number_4.5.py
@@ -1,6 +1,14 @@
 # pseudocode
 # initialize number listing []
+number_list = []
 # us for while loop for input numbers
 # use try to check invalid inputs
+while len(number_list) < 10:
+    try:
+        numbers = int(input("Input number::"))
+        number_list.append(numbers)
+    except ValueError:
+        print("Invalid input. Restart to try again")
+
 # use sum / len fucntions for average
 # print average

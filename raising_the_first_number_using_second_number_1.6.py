d1d665853fd1efad652bd1b9e0f9f94f05ff62a6
Owen <juanmig134187@gmail.com>
2025-03-12 08:32:13 +0800

    user_input_for_numbers_1.6

--- raising_the_first_number_using_second_number_1.6.py
+++ raising_the_first_number_using_second_number_1.6.py
@@ -1,6 +1,14 @@
 # pseudocode 
 # initialization
-# ask user to input two numbers
+
+base_number = 0
+exponent_number = 0
+
+# ask user to input two numbers (float)
+
+base_number = float(input("What's the first number?: "))
+exponent_number = float(input("What's the second number?: "))
+
 # use the second number as exponent and the first as the base
 # base ** exponent
 # print result

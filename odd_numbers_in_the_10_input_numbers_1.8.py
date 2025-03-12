9d29bae38db8442d02d044d241b8fed680bd30a5
Owen <juanmig134187@gmail.com>
2025-03-12 08:54:30 +0800

    input_10_numbers_and_add_to_total_odd_1.8

--- odd_numbers_in_the_10_input_numbers_1.8.py
+++ odd_numbers_in_the_10_input_numbers_1.8.py
@@ -6,4 +6,13 @@ total_odd_numbers = 0
 #use for loop to prompt 10 input numbers
 # if the number input is odd 
     #add 1 to the total_odd_numbers
+
+for numbers in range(10):
+    number_input = float(input(f"Input number {numbers + 1}: "))
+
+    if number_input % 2 != 0:
+        total_odd_numbers += 1
+
+
+
 #print total_odd_numbers

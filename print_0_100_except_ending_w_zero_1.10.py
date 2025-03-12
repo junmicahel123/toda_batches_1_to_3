68a78f76ea441c1a2ec5a37288a1af6df2f7e428
Owen <juanmig134187@gmail.com>
2025-03-12 09:18:05 +0800

    if_decisions_and_printing_of_100_numbers_1.10

--- print_0_100_except_ending_w_zero_1.10.py
+++ print_0_100_except_ending_w_zero_1.10.py
@@ -2,7 +2,10 @@
 # using for loop
  # numbers (0, 100)
 
-for numbers in (0,100):
-
+for numbers in range(101):
 # if number % 10 !=0:
-# print number
+    if numbers % 10 != 0:
+        # print number
+        print(f"Number : {numbers}")
+
+

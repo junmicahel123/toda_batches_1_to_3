5d7019c66f2ff8d1c1542751ecb194994aeefddf
Owen <juanmig134187@gmail.com>
2025-03-12 12:29:11 +0800

    asking_user_for_input_number_2.2

--- not_equal_identifier_2.2.py
+++ not_equal_identifier_2.2.py
@@ -4,6 +4,11 @@ number_1 = 0
 number_2 = 0
 
 # ask user for two number inputs
+
+number_1 = float(input("What's the first number?::"))
+number_2 = float(input("What's the second number?::"))
+
+
 # use if statements
     # if number 1 != to number 2
     # print not equal

55fed1084b64da547509cc3a0b30d912ee527189
Owen <juanmig134187@gmail.com>
2025-03-12 22:00:30 +0800

    while_loop_and_if_decisions_not yet working_3.3

--- Batch 3/display_UNIQUE_DUPLICATE_if_number_is_3.3.py
+++ Batch 3/display_UNIQUE_DUPLICATE_if_number_is_3.3.py
@@ -1,8 +1,22 @@
 # pseudocode 
 # initialize sets number []
 
+number_list = []
+
 # use while loop for numbers input
 # use try and except to look for invalid inputs
 # ask for number
 # use if statements to decide wethe unique or duplicate
+
+while len(number_list) < 10:
+    try:
+        number = int(input("Enter a number::"))
+        if number in number_list:
+            print("UNQUE")
+        else:
+            print("DUPLICATE")
+        number_list.append(number)
+    except ValueError:
+        print("invalid input. TRY AGAIN")
+        break
 #a add to set

066a1443df90bd2de268cf7e766fac99166c558a
Owen <juanmig134187@gmail.com>
2025-03-12 22:38:58 +0800

    user inputs and adding numbers to list _4.1

--- Batch 4/display_all_number_with_duplicates_4.1.py
+++ Batch 4/display_all_number_with_duplicates_4.1.py
@@ -1,9 +1,22 @@
 # pseudocode 
 # initialize number sets []
+
+numbers_list = []
 # use for loop for number input
 # add number input to set using append
+
+for number in range(10):
+    numbers = int(input("Number input ::"))
+    numbers_list.append(numbers)
+
+
 #  add duplicate set
+duplicate_set = []
 # if state ment number cout > 1 
+if numbers_list.count(numbers) > 1:
+    duplicate_set.append(numbers)
+
+    
 # add to duplicate set
 # print the duplicate set
 

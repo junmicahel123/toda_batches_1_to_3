0a2f26eda15edbfc203a691cac5091191268cbdb
Owen <juanmig134187@gmail.com>
2025-03-12 13:47:36 +0800

    pseudocoding_2.7

---
+++ finding_number_of_even_in_10_input_numbers_2.7.py
@@ -0,0 +1,9 @@
+# pseudocode
+# initiaize constant
+    # even_numbers
+# use for loop to input ten numbers
+    # use modulus operation
+    # use if conditions
+    #       if number % 2 != 1
+    # add to even number if true
+# print even_numbers

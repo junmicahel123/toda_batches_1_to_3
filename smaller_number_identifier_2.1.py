58ffd74a34ffc38f8cc95b5f37988331cc642837
Owen <juanmig134187@gmail.com>
2025-03-12 12:24:14 +0800

    ask_user_for_input_number_2.1

--- smaller_number_identifier_2.1.py
+++ smaller_number_identifier_2.1.py
@@ -5,6 +5,11 @@
 
 first_number = 0
 second_number = 0
+
+#ask user to input numbers
+
+first_number = float(input("What's the first number?:"))
+second_number= float(input("What's the second number?:"))
 # use "min" funnctio for (firs_number , second_number)
 
 smaller_number = min(first_number, second_number)
@@ -12,3 +17,4 @@ smaller_number = min(first_number, second_number)
 # print the results
 
 print(f"The smaller number is {smaller_number}")
+

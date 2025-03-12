f4f55624f708c0ccdc95a9c8193eda2aa649e491
Owen <juanmig134187@gmail.com>
2025-03-12 12:54:47 +0800

    show_quotient_of_two_numbers_no_decimals_2.4

--- quotient_of_two_number_no_decimals_2.4.py
+++ quotient_of_two_number_no_decimals_2.4.py
@@ -7,12 +7,14 @@ numero_dos = 0
 
 # ask for two input numbers
 
-numero_uno = input("Input number 1 ::")
-numero_dos = input("Input number 2 ::")
+numero_uno = int(input("Input number 1 ::"))
+numero_dos = int(input("Input number 2 ::"))
 
 
 # find quotient
     # convert to INTEGER to remove decimal
-quotient = int(numero_uno - numero_dos)
+quotient = int(numero_uno / numero_dos)
 
 # print quotient
+
+print(f"The difference of {numero_uno} and {numero_dos} is -->> {quotient}")

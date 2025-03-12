8d50483155ef872edb23e2787ff1c2326f034203
Owen <juanmig134187@gmail.com>
2025-03-12 08:49:54 +0800

    maangas_na_pseudocode_1.8

---
+++ odd_numbers_in_the_10_input_numbers_1.8.py
@@ -0,0 +1,6 @@
+# Pseudocode
+# initialization of "total_odd_numbers"
+#use for loop to prompt 10 input numbers
+# if the number input is odd 
+    #add 1 to the total_odd_numbers
+#print total_odd_numbers

ade40e1029a4305c6de9b593d1f38e765e45e299
Owen <juanmig134187@gmail.com>
2025-03-12 08:30:06 +0800

    pseudocot_this_1.6

--- raising_the_first_number_using_second_number_1.6.py
+++ raising_the_first_number_using_second_number_1.6.py
@@ -1,9 +1,5 @@
 # pseudocode 
 # initialization
-
-base_number = 0
-exponent_number = 0
-
 # ask user to input two numbers
 # use the second number as exponent and the first as the base
 # base ** exponent

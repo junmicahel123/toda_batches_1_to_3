590edbb420da214fd1352bba027c4f4730326564
Owen <juanmig134187@gmail.com>
2025-03-12 23:01:15 +0800

    number input using while loop_4.4

--- Batch 4/display_number_highest_to_lowest_4.4.py
+++ Batch 4/display_number_highest_to_lowest_4.4.py
@@ -1,7 +1,16 @@
 # pseudocode 
 # initialize number list []
+number_list = []
 # use while loop for input
 # use try for checking invalid inputs
+
+while len(number_list) < 10:
+    try:
+        numbers = int(input("Input number::"))
+        number_list.append(numbers)
+    except ValueError:
+        print("Invalid input. Restart to try again")
+        
 
 # reverse the sort func  for the number list
 # print number list

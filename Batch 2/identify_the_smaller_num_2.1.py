b615d0114395fe0f6902118f952d9bcbd6eadaab
Owen <juanmig134187@gmail.com>
2025-03-13 00:36:40 +0800

    pseudocode

---
+++ Batch 2/identify_the_smaller_num_2.1.py
@@ -0,0 +1,8 @@
+# pseudocode
+# initializing constants
+    # first_number = 0
+    # second_number = 0
+#ask user to input numbers
+
+# use "min" funnctio for (firs_number , second_number)
+# print the results

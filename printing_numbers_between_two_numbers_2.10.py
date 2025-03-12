0644826960659a9fd22c6f0980fe64f82f4505ef
Owen <juanmig134187@gmail.com>
2025-03-12 14:44:05 +0800

    user_input_two_numbers_2.10

--- printing_numbers_between_two_numbers_2.10.py
+++ printing_numbers_between_two_numbers_2.10.py
@@ -4,6 +4,10 @@ first_number = 0
 second_number = 0
 
 # ask user for input numbers
+
+first_number = int(input("Input first number:: "))
+second_number = int(input("Input second number:: "))
+
 # use if to make sure
     # if first number is bigger then second
 # print results

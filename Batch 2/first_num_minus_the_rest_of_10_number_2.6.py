d3f8397d9602c66bdb7961a8278af0d4db73f1aa
Owen <juanmig134187@gmail.com>
2025-03-13 00:52:21 +0800

    use sets to first num and the rest the minus = results_2.6

--- Batch 2/first_num_minus_the_rest_of_10_number_2.6.py
+++ Batch 2/first_num_minus_the_rest_of_10_number_2.6.py
@@ -10,3 +10,20 @@
 
 # minus the first number to the iniatilized constant
 # print the results
+
+first_number = 0
+the_rest_of_the_numbers = 0
+
+
+first_number = float(input("Input Number:: "))
+ 
+for the_rest in range(0, 9):
+    numbers_2_9_ = float(input("Input Number:: "))
+    the_rest_of_the_numbers += numbers_2_9_
+
+
+
+difference = first_number - the_rest_of_the_numbers
+
+print(f"The result of difference of the first number minus the rest of the ten numbers \n --> {difference} <--")
+

4f75472b6690cbfe8c5e3c3a8be4eb462ceb28e5
Owen <juanmig134187@gmail.com>
2025-03-12 21:55:56 +0800

    pseudocode_3.3_

---
+++ Batch 3/display_UNIQUE_DUPLICATE_if_number_is_3.3.py
@@ -0,0 +1,8 @@
+# pseudocode 
+# initialize sets number []
+
+# use while loop for numbers input
+# use try and except to look for invalid inputs
+# ask for number
+# use if statements to decide wethe unique or duplicate
+#a add to set

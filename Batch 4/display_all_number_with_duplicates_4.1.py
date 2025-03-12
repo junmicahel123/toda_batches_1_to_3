fb177815d769d9b449c207162c297b4a48cbf980
Owen <juanmig134187@gmail.com>
2025-03-12 22:36:06 +0800

    pseudocode_4.1

---
+++ Batch 4/display_all_number_with_duplicates_4.1.py
@@ -0,0 +1,9 @@
+# pseudocode 
+# initialize number sets []
+# use for loop for number input
+# add number input to set using append
+#  add duplicate set
+# if state ment number cout > 1 
+# add to duplicate set
+# print the duplicate set
+

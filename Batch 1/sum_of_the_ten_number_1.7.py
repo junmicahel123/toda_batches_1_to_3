47969107633fe32715e32ef3f29d447a3ed38697
Owen <juanmig134187@gmail.com>
2025-03-12 23:56:57 +0800

    total sum display_1.7

--- Batch 1/sum_of_the_ten_number_1.7.py
+++ Batch 1/sum_of_the_ten_number_1.7.py
@@ -3,3 +3,11 @@
 # use "for" loop to ask user to input ten numbers
     # the input numbers will be added to the "total_sum"
 # print the total sum of ten numbers
+
+total_sum = 0
+
+for number_input in range(10):
+    numbers = float(input(f" Enter the number {number_input + 1}:  "))
+    total_sum += numbers
+
+print(f" The total sum of all ten numbers is -->>{total_sum}")

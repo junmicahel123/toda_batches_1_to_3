f6479702a0d7c946a10bf04fbee338c9d49aaafc
Owen <juanmig134187@gmail.com>
2025-03-12 22:16:17 +0800

    pseudocode_3.4

---
+++ Batch 3/display_the_lowest_number_3.4.py
@@ -0,0 +1,10 @@
+# pseudocode
+# initialize  number set []
+# use while loop for 10 inputs
+# use try and except for invalid input checking
+
+# ask user for input numbers
+# add number value to set
+
+# use min function
+

4d0ee7545f90d6bb7de2c96434341470404f426c
Owen <juanmig134187@gmail.com>
2025-03-12 23:47:21 +0800

    quotient using / func _1.5

--- Batch 1/quotient_of_two_num_decimals_1.5.py
+++ Batch 1/quotient_of_two_num_decimals_1.5.py
@@ -3,3 +3,11 @@
 # convert to float
 # use / function to find quotient
 # print quotient
+
+
+first_number = float(input("What's the first number?: "))
+second_number = float(input("What's the second number?: "))
+
+quotient = first_number / second_number
+
+print(f"The QUOTIENT of {first_number} and {second_number} is --> {quotient} ")

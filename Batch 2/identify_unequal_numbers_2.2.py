bb6be8f2c35cf1ec21878e424db764616f801229
Owen <juanmig134187@gmail.com>
2025-03-13 00:40:31 +0800

    printing result of NOT EQUAL numbers_2.2

--- Batch 2/identify_unequal_numbers_2.2.py
+++ Batch 2/identify_unequal_numbers_2.2.py
@@ -4,3 +4,16 @@
 # use if statements
     # if number 1 != to number 2
  # print not equal
+
+number_1 = 0
+number_2 = 0
+
+number_1 = float(input("What's the first number?::"))
+number_2 = float(input("What's the second number?::"))
+
+if number_1 != number_2:
+    print("Not Equal")
+else:
+    print("Equal")
+   
+   
